0605a182183d5f0be5da3172027f9f63dc46df19
JPiolho <783467+jpiolho@users.noreply.github.com>
2022-11-09 20:31:27 +0100

    Added some xmldocs to IQuakeGame and IQuakeCallbackReference

--- QuakeReloaded.Interfaces/Controllers/IQuakeGame.cs
+++ QuakeReloaded.Interfaces/Controllers/IQuakeGame.cs
@@ -1,5 +1,8 @@
 ﻿namespace QuakeReloaded.Interfaces
 {
+    /// <summary>
+    /// Provides access to general things from the game.
+    /// </summary>
     public interface IQuakeGame
     {
         float MapTime { get; }
--- QuakeReloaded.Interfaces/IQuakeCallbackReference.cs
+++ QuakeReloaded.Interfaces/IQuakeCallbackReference.cs
@@ -4,6 +4,9 @@ namespace QuakeReloaded.Interfaces
 {
     public interface IQuakeCallbackReference
     {
+        /// <summary>
+        /// Deregisters this callback from the events, so it won't be called anymore
+        /// </summary>
         void Deregister();
     }
 }
