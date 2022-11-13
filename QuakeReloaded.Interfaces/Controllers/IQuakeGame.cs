76b5bd56072e9c593e246a3cb6822dd56afe1dda
JPiolho <783467+jpiolho@users.noreply.github.com>
2022-11-13 19:09:45 +0100

    Added IQuakeGame.Mod and IQuakeGame.ModPath

--- QuakeReloaded.Interfaces/Controllers/IQuakeGame.cs
+++ QuakeReloaded.Interfaces/Controllers/IQuakeGame.cs
@@ -9,5 +9,15 @@
         /// Returns the currently elapsed time in the map, from the client perspective.
         /// </summary>
         float MapTime { get; }
+
+        /// <summary>
+        /// Returns the full path to the current 'game'
+        /// </summary>
+        string ModPath { get; }
+
+        /// <summary>
+        /// Returns the current 'game' that's active. Eg: id1, ctf, hipnotic
+        /// </summary>
+        string Mod { get; }
     }
 }
--- QuakeReloaded/Controllers/QuakeGame.cs
+++ QuakeReloaded/Controllers/QuakeGame.cs
@@ -1,16 +1,18 @@
 ﻿using QuakeReloaded.Interfaces;
 using QuakeReloaded.Utilities;
 using Reloaded.Hooks.Definitions;
+using System.Runtime.InteropServices;
 
 namespace QuakeReloaded.Controllers;
 
 internal class QuakeGame : QuakeControllerBase, IQuakeGame
 {
     private unsafe double* _globalMapTime;
+    private unsafe char** _globalGameDir;
 
     internal QuakeGame(IQuakeReloaded api, IReloadedHooks hooks, QuakeScanner scanner) : base(api, hooks, scanner)
     {
-        // Scan
+        // Scan for global map time
         scanner.Scan("F2 0F 10 15 ?? ?? ?? ?? 0F 28 C2 F2 0F 5E 05 ?? ?? ?? ?? F2 0F 2C D8 6B C3 3C 66 0F 6E C8 F3 0F E6 C9 F2 0F 5C D1 F2 0F 2C FA E8 ?? ?? ?? ??", (mainModule, result) =>
         {
             unsafe
@@ -19,6 +21,35 @@ internal class QuakeGame : QuakeControllerBase, IQuakeGame
                 _globalMapTime = (double*)(mainModule.BaseAddress + result.Offset + offset + 8);
             }
         });
+
+        // Scan for global game dir
+        scanner.Scan("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 41 B9 D1 00 00 00", (mainModule, result) =>
+        {
+            unsafe
+            {
+                int offset = *(int*)(mainModule.BaseAddress + result.Offset + 3);
+                _globalGameDir = (char**)(mainModule.BaseAddress + result.Offset + offset + 7);
+            }
+        });
+    }
+
+    public string ModPath
+    {
+        get
+        {
+            unsafe
+            {
+                return Marshal.PtrToStringUTF8(new nint(*_globalGameDir)) ?? string.Empty;
+            }
+        }
+    }
+
+    public string Mod
+    {
+        get
+        {
+            return Path.GetDirectoryName(ModPath) ?? string.Empty;
+        }
     }
 
     public float MapTime
